45f83baa0b03cc5e902dde1af159c2b3e3df398d
mmmDev <myomaung.dev2008@gmail.com>
2024-02-25 01:52:21 +0630

    [frontend]-api services

---
+++ pandora-fe/src/Config/index.ts
@@ -0,0 +1,19 @@
+export const AppConfig = {
+  BASE_URL: "http://127.0.0.1:8000/api",
+};
+
+export const APIURLS = {
+  users: {
+    signup: "/users/signup",
+    signin: "/users/signin",
+    profile: "/profile",
+    destroy: "/destroy",
+  },
+  post: {
+    create: "/posts/create-post",
+    update: "/posts/update-post",
+    destroy: "/posts/destroy-post",
+    all: "/posts/all-posts",
+    single: "",
+  },
+};
---
+++ pandora-fe/src/Services/index.ts
@@ -0,0 +1,46 @@
+import axios from "axios";
+import { APIURLS, AppConfig } from "../Config";
+
+export const API = axios.create({
+  baseURL: AppConfig.BASE_URL,
+  headers: {
+    Accept: "application/json",
+    "Content-Type": "application/json",
+  },
+});
+
+API.interceptors.request.use((config) => {
+  document.body.classList.add("loader");
+  return config;
+});
+
+API.interceptors.response.use(
+  (response) => {
+    document.body.classList.remove("loader");
+    if (response.config.url?.includes(APIURLS.users.signin)) {
+      const { access_token } = response.data;
+      localStorage.setItem("pandora_access_token", access_token);
+    }
+    return response;
+  },
+  (error) => {
+    document.body.classList.remove("loader");
+    if (error.response) {
+      const { status } = error.response;
+
+      if (status === 401) {
+        localStorage.removeItem("pandora_access_token");
+
+        console.log("Unauthorized error.");
+      } else if (status === 400) {
+        console.log("Bad Request error");
+      }
+    } else if (error.request) {
+      console.log("No response received. Request:", error.request);
+    } else {
+      console.log("Error setting up the request:", error.message);
+    }
+
+    return Promise.reject(error);
+  }
+);
