aac99f5cec9a0c23cb710a06c46bcce106d90855
mmmDev <myomaung.dev2008@gmail.com>
2024-02-25 08:23:57 +0630

    [frontend]-fix type of Post Card

--- pandora-fe/src/Components/Atoms/PostCard.tsx
+++ pandora-fe/src/Components/Atoms/PostCard.tsx
@@ -39,46 +39,48 @@ export const AppPostCard: React.FC<props> = ({ post, refresh }) => {
   };
   return (
     <React.Fragment>
-      <div className="w-full relative group border border-slate-300 bg-white shadow-md px-4 py-4 flex flex-col space-y-2">
-        {user && isAuth && user.id === post.user.id ? (
-          <React.Fragment>
-            <div className="absolute top-0 right-0 flex flex-row space-x-2">
-              <Link
-                to={`/edit-post/${post.id}`}
-                className=" bg-secondary hover:bg-primary right-0 px-2 py-2"
-              >
-                <RiEditLine className="w-auto h-6 text-white" />
-              </Link>
-              <button
-                type="button"
-                onClick={() => handleDestroyPost()}
-                className=" bg-secondary hover:bg-primary right-0 px-2 py-2"
-              >
-                <RiDeleteBinLine className="w-auto h-6 text-white" />
-              </button>
-            </div>
-          </React.Fragment>
-        ) : null}
-        <Link to={`/post/${post.id}`}>
-          <React.Fragment>
-            <h6 className="text-lg font-bold text-secondary underline underline-offset-8 ">
-              {post.title}
-            </h6>
-            <div className="px-2 py-2">
-              <p className="text-sm font-normal text-slate-800">
-                {post.content}
-              </p>
-            </div>
-            <hr />
-            <div className="w-full flex flex-row justify-end items-center content-center">
-              <RiHistoryLine className="w-auto h-4 text-slate-600" />
-              <span className="text-xs text-primary font-thin">
-                {moment(new Date(post.updated_at)).fromNow()}
-              </span>
-            </div>
-          </React.Fragment>
-        </Link>
-      </div>
+      {post ? (
+        <div className="w-full relative group border border-slate-300 bg-white shadow-md px-4 py-4 flex flex-col space-y-2">
+          {user && isAuth && user.id === post.user?.id ? (
+            <React.Fragment>
+              <div className="absolute top-0 right-0 flex flex-row space-x-2">
+                <Link
+                  to={`/edit-post/${post.id}`}
+                  className=" bg-secondary hover:bg-primary right-0 px-2 py-2"
+                >
+                  <RiEditLine className="w-auto h-6 text-white" />
+                </Link>
+                <button
+                  type="button"
+                  onClick={() => handleDestroyPost()}
+                  className=" bg-secondary hover:bg-primary right-0 px-2 py-2"
+                >
+                  <RiDeleteBinLine className="w-auto h-6 text-white" />
+                </button>
+              </div>
+            </React.Fragment>
+          ) : null}
+          <Link to={`/post/${post.id}`}>
+            <React.Fragment>
+              <h6 className="text-lg font-bold text-secondary underline underline-offset-8 ">
+                {post.title}
+              </h6>
+              <div className="px-2 py-2">
+                <p className="text-sm font-normal text-slate-800">
+                  {post.content}
+                </p>
+              </div>
+              <hr />
+              <div className="w-full flex flex-row justify-end items-center content-center">
+                <RiHistoryLine className="w-auto h-4 text-slate-600" />
+                <span className="text-xs text-primary font-thin">
+                  {moment(new Date(post.updated_at)).fromNow()}
+                </span>
+              </div>
+            </React.Fragment>
+          </Link>
+        </div>
+      ) : null}
     </React.Fragment>
   );
 };
